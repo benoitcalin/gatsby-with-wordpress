bda7c8823caa599edc1538cd7385b04431081b1f
Benoit Calin <benoit.calin@gmail.com>
2019-08-23 17:45:14 +0200

    header + form

--- src/components/header.js
+++ src/components/header.js
@@ -1,31 +1,88 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
+import { Link, StaticQuery, graphql } from "gatsby"
+import Img from 'gatsby-image'
 import React from "react"
 
-const Header = ({ siteTitle }) => (
-  <div className='header'>
-    <div className='header-container'>
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
-)
+const Header = () => {
+  const handleClick = (event) => {
+    event.currentTarget.classList.toggle('open')
+  }
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+  const coloredUrl = (relativeUrl) => {
+    if (window.location.pathname === relativeUrl) {
+      return 'colored-link'
+    }
+  }
 
-Header.defaultProps = {
-  siteTitle: ``,
+  return (
+    <StaticQuery
+      query={QUERY}
+      render={data => {
+        const logo = data.wpgraphql.page.page_home.logo
+
+        return (
+          <div className='header'>
+            <div className="header-logo">
+              <Img
+                fluid={logo.imageFile.childImageSharp.fluid}
+                alt={logo.alt}
+                loading='auto'
+              />
+            </div>
+            <div className='header-links'>
+              <Link className={coloredUrl('/')} to='/'>CONCEPT</Link>
+              <Link className={coloredUrl('/jobs')} to='/jobs'>METIERS</Link>
+              <Link className={coloredUrl('/contact')} to='/jobs'>CONTACT</Link>
+              <Link className={coloredUrl('/partnership')} to='/jobs'>DEVENIR PARTENAIRE</Link>
+            </div>
+            <div
+              id="nav-icon1"
+              onClick={handleClick}
+              // type="button"
+              className="navbar-toggle collapsed"
+              data-toggle="collapse"
+              data-target="#navbar-main-collapse"
+              aria-expanded="false"
+            >
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+            <div
+              className="collapse navbar-collapse"
+              id="navbar-main-collapse"
+            >
+              {/* <Link>CONCEPT</Link>
+              <Link>METIERS</Link>
+              <Link>CONTACT</Link>
+              <Link>DEVENIR PARTENAIRE</Link> */}
+            </div>
+          </div>
+        )
+      }}
+    />
+  )
 }
 
 export default Header
+
+const QUERY = graphql`
+  query GET_LOGO {
+    wpgraphql {
+      page(id: "cGFnZToxNjc=") {
+        page_home {
+          logo {
+            altText
+            sourceUrl
+            imageFile {
+              childImageSharp {
+                fluid(maxWidth: 1000) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
--- src/templates/form.js
+++ src/templates/form.js
@@ -1,14 +1,134 @@
 import React from "react"
-// import { Link } from "gatsby"
+import Layout from "../components/layout";
 
-import Layout from "../components/layout"
-// import SEO from "../components/seo"
+import 'flatpickr/dist/flatpickr.min.css';
+import flatpickr from "flatpickr";
 
-const Form = (props) => (
-  <Layout>
-    <h1>{props.pageContext.slug}</h1>
-    <h5>{props.pageContext.title}</h5>
-  </Layout>
-)
-
-export default Form
+flatpickr("#myID", {
+  mode: "range",
+  minDate: "today"
+})
+export default class Form extends React.Component {
+  state = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    date: "",
+    content: "",
+  }
+  handleInputChange = event => {
+    const target = event.target
+    const value = target.value
+    const name = target.name
+    this.setState({
+      [name]: value,
+    })
+  }
+  handleSubmit = event => {
+    event.preventDefault()
+    alert(`Welcome ${this.state.firstName} ${this.state.lastName}!`)
+  }
+  render() {
+    return (
+      <Layout>
+        <div className="form-title">
+          <h2 style={{margin: '50px 0'}}>Nous contacter</h2>
+        </div>
+        <div className='form-container'>
+          <form
+            className='row form-row'
+            action="https://formspree.io/benoit.calin@gmail.com"
+            method="POST"
+          >
+            <div className="col-12 col-md-6 form-group">
+              <label>
+                Prénom<span style={{ color: 'red' }}>*</span>
+              </label>
+              <input
+                className="form-control"
+                type="text"
+                name="firstName"
+                value={this.state.firstName}
+                onChange={this.handleInputChange}
+                required
+              />
+            </div>
+            <div className="col-12 col-md-6 form-group">
+              <label>
+                Nom<span style={{ color: 'red' }}>*</span>
+              </label>
+              <input
+                className="form-control"
+                type="text"
+                name="lastName"
+                value={this.state.lastName}
+                onChange={this.handleInputChange}
+                required
+              />
+            </div>
+            <div className="col-12 col-md-6 form-group">
+              <label>
+                Email<span style={{color: 'red'}}>*</span>
+              </label>
+              <input
+                className="form-control"
+                type="text"
+                name="email"
+                value={this.state.email}
+                onChange={this.handleInputChange}
+                required
+              />
+            </div>
+            <div className="col-12 col-md-6 form-group">
+              <label>
+                Téléphone
+              </label>
+              <input
+                className="form-control"
+                type="text"
+                name="phone"
+                value={this.state.phone}
+                onChange={this.handleInputChange}
+              />
+            </div>
+            <div className="col-12 form-group">
+              <label>
+                Disponibilité
+              </label>
+              <input
+                className="form-control"
+                type="text"
+                id='myID'
+                name="disponibility"
+                placeholder='Choisissez la période de votre choix'
+                value={this.state.date}
+                onChange={this.handleInputChange}
+              />
+            </div>
+            <div className="col-12 form-group">
+              <label>
+                Parlez-nous de votre projet
+              </label>
+              <textarea
+                className="textarea"
+                type="text"
+                name="content"
+                value={this.state.content}
+                onChange={this.handleInputChange}
+              />
+            </div>
+            <div className="submit-zone">
+              <p><span style={{ color: 'red' }}>*</span> Ces champs sont obligatoires</p>
+              <div className='d-flex align-items-center' style={{marginBottom: '16px'}}>
+                <input type="checkbox" name="CGU" required/>
+                <p style={{ margin: '0 0 0 15px' }}>J'ai prix connaissance et accepte CGU.<span>*</span></p>
+              </div>
+              <button type="submit" className='btn button-primary'>ENVOYER</button>
+            </div>
+          </form>
+        </div>
+      </Layout>
+    )
+  }
+}
