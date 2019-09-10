6010ad9119e1883f9a7c65eefb2d757e8b1aabe5
Benoit Calin <benoit.calin@gmail.com>
2019-09-10 15:09:44 +0200

    Get recomendations better

--- src/components/recomendation.js
+++ src/components/recomendation.js
@@ -3,28 +3,30 @@ import Img from 'gatsby-image';
 
 const Recomendation = (props) => {
   return (
-    <>
-      <div className="yellow-bar"></div>
-      <div className="recomendation-container">
-        <div className="recomendation-title">
-          <Img
-            fluid={props.image}
-            className='recomendation-image'
-          />
-          <div className="recomendation-person">
-            <h5>{props.name}</h5>
-          </div>
+    <div className="recomendation-container">
+      <div className="recomendation-title">
+        <Img
+          fluid={props.image}
+          className='recomendation-image'
+        />
+        <div className="recomendation-person">
+          <h5>{props.name}</h5>
         </div>
+      </div>
+      <div className='recomendation-quote-cont'>
         <img
           src="https://metroboulododo.fr/wp-content/uploads/2019/09/quotes.png"
           alt='quotes'
           className="quotes recomendation-quotes"
         />
+      </div>
+      <div className='recomendation-text'>
         <p className='text-center'>{props.text1}</p>
+        <br />
         <p className='text-center'><strong>{props.text2}</strong></p>
         <p className='text-center'>{props.text3}</p>
       </div>
-    </>
+    </div>
   )
 }
 
